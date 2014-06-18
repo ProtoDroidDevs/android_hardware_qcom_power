58e6b66a817f6c0c273e93a0c350bcdcedbe1f67
Albert Moravec <albert.moravec@gmail.com>
2014-06-18 21:30:04 +0200

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,27 @@
+ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+ifneq ($(TARGET_USES_CM_POWERHAL),true)
+ifneq ($(TARGET_PROVIDES_POWERHAL),true)
+
+LOCAL_PATH := $(call my-dir)
+
+# HAL module implemenation stored in
+# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
+include $(CLEAR_VARS)
+
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+LOCAL_SRC_FILES := \
+                   power.c \
+
+LOCAL_MODULE:= power.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+
+ifneq ($(TARGET_MPDECISION_BOOST_SOCKET),)
+LOCAL_CFLAGS += -DBOOST_SOCKET=\"$(TARGET_MPDECISION_BOOST_SOCKET)\"
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+endif
+endif
+endif
